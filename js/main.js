df36a8ebb9aa2b8d14d91c6a0be742813c4a8904
Freezard <Freezard@gmail.com>
2015-04-03 14:43:55 +0200

    Fixes #22, fixes #23
    -Fixed card list sorting algorithm
    -Code cleanup
    -Changed free quality color
    -Added back golden cards with new styling
    -Added more data

--- js/main.js
+++ js/main.js
@@ -1,37 +1,7 @@
+/*  main.js
+    Hearthstone Collection Tracker
+*/
 var HSCollectionTracker = (function() {
-
-	function handleKey(element, event) {
-	  var a = String.fromCharCode(event.keyCode);
-	  console.log(a); 
-	  console.log(element.parentNode.innerText.split(" ")[0]);
-	  if (/\d/.test(a)) {
-		  if (window.getSelection().type == "Range")
-			  if (element.innerHTML.length <= 2)
-				  return true;
-			  else return false;
-		  
-		  else if (element.innerHTML.length + 1 <= 2)
-			return true;
-		}
-	  return false;
-	}
-	function lol(e) {
-	   window.getSelection().removeAllRanges();
-	   /*document.selection.empty()*/
-	   console.log(e);
-		//e.innerHTML = "lol";
-	}
-	/*$('span[contenteditable]').keydown(function(e) {
-		// trap the return key being pressed
-		if (e.keyCode === 13) {
-		  // insert 2 br tags (if only one br tag is inserted the cursor won't go to the next line)
-		  //document.execCommand('insertHTML', false, '<br><br>');
-		  document.getElementById("a").blur();
-		  // prevent the default behaviour of return key pressed
-		  return false;
-		}
-	  });*/
-	  
 	var classesEnum = {
 		neutral: "neutral",
 		druid: "druid",
@@ -52,7 +22,8 @@ var HSCollectionTracker = (function() {
 		epic: "epic",
 		legendary: "legendary"
 	};
-		
+	
+	// Values = the number of cards in each set
 	var setsEnum = {
 		basic: 133,
 		classic: 245,
@@ -63,6 +34,7 @@ var HSCollectionTracker = (function() {
 		blackrock: 31
 	};
 
+	// Values = normal and golden cards
 	var missingCardsTotal = {
 			free: [0, 0],
 			common: [0, 0],
@@ -73,25 +45,25 @@ var HSCollectionTracker = (function() {
 	};
 
 	var classes = null;
-
 	var selectedClass = "neutral";
 	var selectedCardQuality = "normal";
-		
-	function card(name, rarity, mana, type, set, soulbound) {
+	
+	function card(name, rarity, mana, type, className, set, soulbound) {
 		this.name = name;
 		this.rarity = rarity;
 		this.mana = mana;
 		this.type = type;
+		this.className = className;
 		this.set = set;
 		this.soulbound = soulbound;
 		this.normal = 0;
 		this.golden = 0;
 	}
 		
-	function class_(name) {
+	function classHS(name) {
 		this.name = name;
 		this.level = 1;
-		this.cards = { 
+		this.cards = {
 			free: {},
 			common: {},
 			rare: {},
@@ -134,24 +106,38 @@ var HSCollectionTracker = (function() {
 		}
 	}
 
-	function sortCards(b) {
-		var cardList = classes[b].cards;
+	// Sorts all the card lists for the specified class
+	// Sorting order: Mana cost: Lower > higher
+	// Type: Weapon > spell > minion
+	// Name: Lexicographical order
+	function sortCards(className) {
+		var cardList = classes[className].cards;
+		
 		for (var rarity in cardList) {
-			var sortedList = [];
-			for (var a in cardList[rarity]) {
-				sortedList.push([a, cardList[rarity][a]]);
+			var sortedArray = [];
+			for (var name in cardList[rarity]) {
+				sortedArray.push([name, cardList[rarity][name]]);
 			}
-			sortedList.sort(function(a, b) { return a[1].mana == b[1].mana ? a[1].name.localeCompare(b[1].name) : a[1].mana - b[1].mana });
+			sortedArray.sort(function(a, b) { 
+			    return a[1].mana == b[1].mana ?
+				a[1].type === b[1].type ?
+				a[1].name.localeCompare(b[1].name) :
+				a[1].type === "weapon" ? -1 :
+				a[1].type === "spell" ? -1 :
+				1 :	a[1].mana - b[1].mana
+				});
+			
+			var sortedList = {};
+			for (var i = 0; i < sortedArray.length; ++i)
+				if (sortedArray[i] !== undefined)
+					sortedList[sortedArray[i][0]] = sortedArray[i][1];
 		
-			var rv = {};
-			for (var i = 0; i < sortedList.length; ++i)
-				if (sortedList[i] !== undefined)
-					rv[sortedList[i][0]] = sortedList[i][1];
-		
-			cardList[rarity] = rv;
+			cardList[rarity] = sortedList;
 		}
 	}
-		
+	
+	// Adds a card through clicking on an <li><a> element
+	// Not finished
 	function addCard(element, card) {
 		card[selectedCardQuality]++;
 		
@@ -168,31 +154,31 @@ var HSCollectionTracker = (function() {
 			missingCardsTotal[card.rarity][1]--;
 		}
 		
-		localStorage.setItem('testObject', JSON.stringify(classes));
-		localStorage.setItem('total', JSON.stringify(missingCardsTotal));
+		localStorage.setItem('classes', JSON.stringify(classes));
+		localStorage.setItem('missingCardsTotal', JSON.stringify(missingCardsTotal));
 		
-		var b = card.rarity.charAt(0).toUpperCase() + card.rarity.slice(1);
-		var c = selectedCardQuality.charAt(0).toUpperCase() + selectedCardQuality.slice(1);
-		var td = document.getElementById("classMissing" + b + c);
+		var rarityCapitalized = card.rarity.charAt(0).toUpperCase() + card.rarity.slice(1);
+		var cardQualityCapitalized = selectedCardQuality.charAt(0).toUpperCase() + selectedCardQuality.slice(1);
+		var td = document.getElementById("classMissing" + rarityCapitalized + cardQualityCapitalized);
 		td.innerHTML -= 1;
-		td = document.getElementById("totalMissing" + b + c);
+		td = document.getElementById("totalMissing" + rarityCapitalized + cardQualityCapitalized);
 		td.innerHTML -= 1;
 		
 		return true;
 	}
-		
+	
 	function displayClassTabs() {
 		var div = document.getElementById("classTabs");
 		var list = document.createElement("ul");
 		document.getElementById("classTabsBar").setAttribute("class", selectedClass);
 			
-		for (var class__ in classes) {
+		for (var className in classes) {
 			var listItem = document.createElement("li");
 			var listItemLink = document.createElement("a");
 			var span = document.createElement("span");
-			span.innerHTML = classes[class__].level;
+			span.innerHTML = classes[className].level;
 				
-			span.setAttribute("contenteditable", true);
+			//span.setAttribute("contenteditable", true);
 			/* BECAUSE EVENT LISTENERS NO WORKY ON THIS SPAN.
 			   USE FORM INSTEAD? */
 			span.setAttribute("onkeypress", "return handleKey(this, event)");
@@ -201,28 +187,36 @@ var HSCollectionTracker = (function() {
 			span.setAttribute("ondrop", "return false");
 			/* --------------------------------------------- */
 			listItemLink.appendChild(span);
-			if (class__ === "neutral")
-				listItemLink.innerHTML = class__;
+			
+			if (className === "neutral")
+				listItemLink.innerHTML = className;
 			else {
-				listItemLink.innerHTML = class__ + "<br>(" + listItemLink.innerHTML;
+				listItemLink.innerHTML = className + "<br>(" + listItemLink.innerHTML;
 				listItemLink.innerHTML += ")";
 			}
-			(function (class__) {
-				listItemLink.addEventListener("click", function() { document.getElementById("classTabsBar").setAttribute("class", class__); selectedClass = class__; displayCards(class__); displayMissingCards(); });
-				}(class__))
-				listItemLink.setAttribute("class", class__ + " " + "noselect");
+			
+			(function (className) {
+				listItemLink.addEventListener("click", function() {
+					document.getElementById("classTabsBar").setAttribute("class", className);
+					selectedClass = className;
+					displayCards(className);
+					displayMissingCards();
+				});
+			}(className))
+			
+			listItemLink.setAttribute("class", className + " " + "noselect");
 				
-				listItem.appendChild(listItemLink);
-				list.appendChild(listItem);
+			listItem.appendChild(listItemLink);
+			list.appendChild(listItem);
 		}
 		div.appendChild(list);
 	}
-		
-	function displayCards(a) {
-		var cardList = classes[a].cards;
+	
+	function displayCards(className) {
+		var cardList = classes[className].cards;
 			
-		for (var a in raritiesEnum) {
-			var list = document.getElementById("list_" + a);		
+		for (var className in raritiesEnum) {
+			var list = document.getElementById("list_" + className);		
 			while (list.firstChild) {
 				list.removeChild(list.firstChild);
 			}
@@ -232,64 +226,64 @@ var HSCollectionTracker = (function() {
 		
 		for (var rarity in cardList) {		    
 			var list = document.getElementById("list_" + rarity);
-			for (var card in cardList[rarity]) {
-				var card__ = cardList[rarity][card];
+			for (var name in cardList[rarity]) {
+				var card = cardList[rarity][name];
 				var listItem = document.createElement("li");
 				var listItemLink = document.createElement("a");
-				listItemLink.textContent = card;
-				(function (card__) {
-					listItemLink.addEventListener("click", function() { addCard(this, card__); });
-					}(card__))
-					listItemLink.setAttribute("class", "normal" + cardList[rarity][card].normal + " " + "golden" + cardList[rarity][card].golden + " " + "noselect");
+				listItemLink.textContent = name;
+				(function (card) {
+					listItemLink.addEventListener("click", function() { addCard(this, card); });
+					}(card))
+				listItemLink.setAttribute("class", "normal" + cardList[rarity][name].normal + " " + "golden" + cardList[rarity][name].golden + " " + "noselect");
 			
-					listItem.appendChild(listItemLink);
-					list.appendChild(listItem);
+				listItem.appendChild(listItemLink);
+				list.appendChild(listItem);
 			}
 		}
 	}
-		
+	
 	function displayMissingCards() {
-		document.getElementById("classMissingTitle").innerHTML = selectedClass.toUpperCase();
-		for (var a in classes[selectedClass].missingCards) {
-			var b = a.charAt(0).toUpperCase() + a.slice(1);
-			var td = document.getElementById("classMissing" + b + "Normal");
-			var normal = classes[selectedClass].missingCards[a][0];
+		document.getElementById("missingCardsClassTitle").innerHTML = selectedClass.toUpperCase();
+		
+		for (var rarity in classes[selectedClass].missingCards) {
+			var rarityCapitalized = rarity.charAt(0).toUpperCase() + rarity.slice(1);
+			var td = document.getElementById("classMissing" + rarityCapitalized + "Normal");
+			var normal = classes[selectedClass].missingCards[rarity][0];
 			td.innerHTML = normal;
-			td = document.getElementById("classMissing" + b + "Golden");
-			td.innerHTML = classes[selectedClass].missingCards[a][1];
+			td = document.getElementById("classMissing" + rarityCapitalized + "Golden");
+			td.innerHTML = classes[selectedClass].missingCards[rarity][1];
 		}
 	}
 
 	function displayMissingCardsTotal() {
-		for (var a in missingCardsTotal) {
-			var b = a.charAt(0).toUpperCase() + a.slice(1);
-			var td = document.getElementById("totalMissing" + b + "Normal");
-			var normal = missingCardsTotal[a][0];
+		for (var rarity in missingCardsTotal) {
+			var rarityCapitalized = rarity.charAt(0).toUpperCase() + rarity.slice(1);
+			var td = document.getElementById("totalMissing" + rarityCapitalized + "Normal");
+			var normal = missingCardsTotal[rarity][0];
 			td.innerHTML = normal;
-			td = document.getElementById("totalMissing" + b + "Golden");
-			td.innerHTML = missingCardsTotal[a][1];	
+			td = document.getElementById("totalMissing" + rarityCapitalized + "Golden");
+			td.innerHTML = missingCardsTotal[rarity][1];	
 		}
 	}
 
 	function initializeClasses() {
 		classes = {};
-		for (var class__ in classesEnum)
-			classes[class__] = new class_(class__);
+		for (var className in classesEnum)
+			classes[className] = new classHS(className);
 	}
-		
+	
 	function importCards(file) {
 		var cardData = readTextFile(file);
 		cardData = cardData.replace(/(\r)/gm, "");
 		var cardLines = cardData.split("\n");
 		var cardClass;
-		var i;
-		for (i = 0; i < cardLines.length; i++) {
+		for (var i = 0; i < cardLines.length; i++) {
 			var cardLine = cardLines[i].split(",");
 			if (cardLine.length == 1 && cardLine != "") {
 				cardClass = cardLine[0];
 			}
 			else if (cardLine != "") {
-				classes[cardClass].addCard(new card(cardLine[0], cardLine[1], cardLine[2]));
+				classes[cardClass].addCard(new card(cardLine[0], cardLine[1], cardLine[2], cardLine[3]));
 			}
 		}
 	}
@@ -319,11 +313,11 @@ var HSCollectionTracker = (function() {
 	}
 
 	function initSelectedCardQuality() {		
-		var a = document.getElementById("selectedQualityNormal");
-		var b = document.getElementById("selectedQualityGolden");
+		var selectedQualityNormal = document.getElementById("selectedQualityNormal");
+		var selectedQualityGolden = document.getElementById("selectedQualityGolden");
 		
-		a.addEventListener("click", function() { setSelectedCardQuality(this); });
-		b.addEventListener("click", function() { setSelectedCardQuality(this); });
+		selectedQualityNormal.addEventListener("click", function() { setSelectedCardQuality(this); });
+		selectedQualityGolden.addEventListener("click", function() { setSelectedCardQuality(this); });
     }
 
     function setSelectedCardQuality(element) {
@@ -335,46 +329,81 @@ var HSCollectionTracker = (function() {
 		element.setAttribute("class", "selected");
     }
 	
+	// Not done
+	function handleKey(element, event) {
+	  var a = String.fromCharCode(event.keyCode);
+	  console.log(a); 
+	  console.log(element.parentNode.innerText.split(" ")[0]);
+	  if (/\d/.test(a)) {
+		  if (window.getSelection().type == "Range")
+			  if (element.innerHTML.length <= 2)
+				  return true;
+			  else return false;
+		  
+		  else if (element.innerHTML.length + 1 <= 2)
+			return true;
+		}
+	  return false;
+	}
+	
+	function lol(e) {
+	   window.getSelection().removeAllRanges();
+	   /*document.selection.empty()*/
+	   console.log(e);
+		//e.innerHTML = "lol";
+	}
+	
+	/*$('span[contenteditable]').keydown(function(e) {
+		// trap the return key being pressed
+		if (e.keyCode === 13) {
+		  // insert 2 br tags (if only one br tag is inserted the cursor won't go to the next line)
+		  //document.execCommand('insertHTML', false, '<br><br>');
+		  document.getElementById("a").blur();
+		  // prevent the default behaviour of return key pressed
+		  return false;
+		}
+	  });*/
+	
 	return {
-		init: function() {	
-		//localStorage.clear();
-		console.log(JSON.stringify(localStorage).length);
-		
-		if (typeof(Storage) !== "undefined") {
-			var retrievedObject = localStorage.getItem('testObject');
-			classes = JSON.parse(retrievedObject);
+		init: function() {
+			//localStorage.clear();
+			//console.log(JSON.stringify(localStorage).length);
+			
+			if (typeof(Storage) !== "undefined") {
+				var retrievedObject = localStorage.getItem('classes');
+				classes = JSON.parse(retrievedObject);
 
-		} else {
-			// Sorry! No Web Storage support..
-		}	
-		
-		if (classes == null) {
-			initializeClasses();
+			} else {
+				// Sorry! No Web Storage support..
+			}	
 			
-			importCards("basic.txt");
-			importCards("classic.txt");
-			importCards("reward.txt");
-			importCards("promo.txt");
-			importCards("naxxramas.txt");
-			
-			for (var iii in classes) {
-				sortCards(iii);
+			if (classes == null) {
+				initializeClasses();
+				
+				importCards("basic.txt");
+				importCards("classic.txt");
+				importCards("reward.txt");
+				importCards("promo.txt");
+				importCards("naxxramas.txt");
+				
+				for (var className in classes) {
+					sortCards(className);
+				}
+				
+				localStorage.setItem('classes', JSON.stringify(classes));
+				localStorage.setItem('missingCardsTotal', JSON.stringify(missingCardsTotal));
+			}
+			else {
+				var temp = localStorage.getItem("missingCardsTotal");
+				missingCardsTotal = JSON.parse(temp);
 			}
 			
-			localStorage.setItem('testObject', JSON.stringify(classes));
-			localStorage.setItem('total', JSON.stringify(missingCardsTotal));
+			initSelectedCardQuality();
+			displayClassTabs();
+			displayCards(selectedClass);
+			displayMissingCards();
+			displayMissingCardsTotal();
 		}
-		else {
-			var fei = localStorage.getItem("total");
-			missingCardsTotal = JSON.parse(fei);
-		}
-		
-		initSelectedCardQuality();
-		displayClassTabs();
-		displayCards(selectedClass);
-		displayMissingCards();
-		displayMissingCardsTotal();
-	    }
 	};
 })();
 
