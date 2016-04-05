528d4360ddf6335a227039236f8ab424ea2079a5
Leonid Vakulenko <leonid.vakulenko@webasyst.net>
2016-04-05 18:20:57 +0300

    Apply tooltip even after switching to another tab and back

--- js/main.js
+++ js/main.js
@@ -388,7 +388,10 @@ var HSCollectionTracker = (function() {
 			var href_tmpl = 'http://www.hearthpwn.com/cards/%d';
 
 			// When list of cards changes, reinit the tooltips
-			$('#classCards').on('listchange', function() {
+			$(document).on('listchange listinit', '#classCards', applyTooltips);
+			applyTooltips();
+
+			function applyTooltips() {
 				CurseTips['hearth-tooltip'].watchElements($('#classCards li a:not(.buttonAll)').map(function(a) {
 					var $a = $(this);
 					var card_name = $a.text();
@@ -399,7 +402,7 @@ var HSCollectionTracker = (function() {
 					$li.attr('data-tooltip-href', href_tmpl.replace('%d', card_ids[card_name]));
 					return $li[0];
 				}).get());
-			}).trigger('listchange');
+			}
 		});
 	}
 
